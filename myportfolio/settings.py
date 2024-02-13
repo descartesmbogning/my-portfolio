98d4cefb3e3860cb4b2ca894f0e2e5593fd0fe18
Maxime Descartes MBOGNING FONKOU <71991368+descartesmbogning@users.noreply.github.com>
2024-02-13 07:12:17 +0100

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # If you have a global static directory, add it here
 STATICFILES_DIRS = [
